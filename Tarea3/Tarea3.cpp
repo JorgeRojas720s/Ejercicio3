a2ce2044c426913bd6ec7d4d4875d45788031aa9
Jorge Rojas <jitorojas37@gmail.com>
2023-02-07 03:22:50 -0600

    first option complete and an extra function pressEnterToContinue, and added the files

--- Tarea3/Tarea3.cpp
+++ Tarea3/Tarea3.cpp
@@ -6,6 +6,66 @@
 
 using namespace std;
 
+void menu();
+void pressEnterToContinue();
+void readTextAndCountWords();
+void countWords(ifstream *, string);
+
+int main() {
+
+    setlocale(LC_ALL, "es_ES.UTF-8");
+
+    menu();
+
+    return 0;
+}
+
+void countWords(ifstream *file,string text) {
+
+    int wordCount = 1;
+
+    if (file->is_open()) {
+
+        while (getline(*file, text)) {
+            cout << text << endl;
+            for (int i = 0; i < text.length(); i++) {
+                if (text[i] == ' ') {
+                    wordCount++;
+                }
+            }
+        }
+        file->close();
+    }
+    cout << "\n\nNumber of words: " << wordCount;
+
+}
+
+void readTextAndCountWords() {
+
+    ifstream *file = new ifstream();
+    string text;
+
+    file->open("archivo.txt", ios::in);
+
+    if (file->fail()) {
+        cout << "Could not open the file";
+        pressEnterToContinue();
+        menu();
+    }
+
+    countWords(file,text);
+ 
+
+}
+
+void pressEnterToContinue() {
+    cin.clear();
+    cin.ignore();
+    cout << "\n\n\nPRESS ENTER TO CONTINUE..." << endl;
+    getchar();
+    system("cls");
+}
+
 void menu() {
 
     int option;
@@ -16,63 +76,33 @@ void menu() {
         cout << "\n\t(2) Generate a new file with reversed words";
         cout << "\n\t(3) Join CSV files";
         cout << "\n\t(4) Find word and change it";
-        cout << "\n\t(5) Exit" << endl;
-        cout << "Option => ";
+        cout << "\n\t(5) Exit";
+        cout << "\n\tOption => ";
         cin >> option;
-
+        system("cls");
         switch (option) {
 
-        case 1: cout << endl << "La 1";
-            cin.clear();
-            cin.ignore();
-            cout << "\nPRESS ENTER TO CONTINUE..." << endl;
-            getchar();
-            system("cls");
+        case 1: cout << "\n"; readTextAndCountWords();
+            pressEnterToContinue();
             break;
         case 2:cout << endl << "La 2";
-            cin.clear();
-            cin.ignore();
-            cout << "\nPRESS ENTER TO CONTINUE..." << endl;
-            getchar();
-            system("cls");
+            pressEnterToContinue();
             break;
         case 3:cout << endl << "La 3";
-            cin.clear();
-            cin.ignore();
-            cout << "\nPRESS ENTER TO CONTINUE..." << endl;
-            getchar();
-            system("cls");
+            pressEnterToContinue();
             break;
         case 4:cout << endl << "La 4";
-            cin.clear();
-            cin.ignore();
-            cout << "\nPRESS ENTER TO CONTINUE..." << endl;
-            getchar();
-            system("cls");
+            pressEnterToContinue();
             break;
-        case 5:cout << endl << "La 5";
-            cin.clear();
-            cin.ignore();
-            cout << "\nPRESS ENTER TO CONTINUE..." << endl;
-            getchar();
-            system("cls");
+        case 5: cout << "\n Bye :)";
+            exit(0);
+
             break;
         default: cout << endl << "Invalid option";
-            cin.clear();
-            cin.ignore();
-            cout << "\nPRESS ENTER TO CONTINUE..." << endl;
-            getchar();
-            system("cls");
+            pressEnterToContinue();
 
         }
     } while (option < 6);
     menu();
 
-}
-
-int main() {
-
-    menu();
-
-    return 0;
 }
